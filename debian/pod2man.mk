0126b769bb77acdbc129bc2ca85f8d74fd09d04e
Jari Aalto <jari.aalto@cante.net>
2008-10-27 07:45:26 +0200

    debian/*.mk: update
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- debian/pod2man.mk
+++ debian/pod2man.mk
@@ -1,37 +1,61 @@
-# Copyright (C) 2005-2007 Jari Aalto
+# pod2man.mk -- Makefile portion to convert *.pod files to manual pages
 #
-# This program is free software; you can redistribute it and or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of
-# the License, or (at your option) any later version.
+#   Copyright information
 #
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details at
-# <http://www.gnu.org/copyleft/gpl.html>
+#	Copyright (C) 2008-2009 Jari Aalto
 #
-# Put this to 'install' target:
+#   License
 #
-#   install: build $(MANPAGE)
+#	This program is free software; you can redistribute it and/or
+#	This program is free software; you can redistribute it and/or
+#	modify it under the terms of the GNU General Public License as
+#	published by the Free Software Foundation; either version 2 of the
+#	License, or (at your option) any later version
+#
+#	This program is distributed in the hope that it will be useful, but
+#	WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#	General Public License for more details at
+#	Visit <http://www.gnu.org/copyleft/gpl.html>.
+#
+#   Description
+#
+# 	Convert *.pod files to manual pages. Write this to 'install'
+# 	target:
+#
+#       install: build $(MANPAGE)
 
+ifneq (,)
+This makefile requires GNU Make.
+endif
+
+# These variables *must* be set when calling
 PACKAGE		= package
 PODCENTER	= $$(date "+%Y-%m-%d")
 MANSECT		= 1
+
+# Directories
+MANSRC		=
 MANDEST		= $(MANSRC)
 
 MANPOD		= $(MANSRC)$(PACKAGE).$(MANSECT).pod
 MANPAGE		= $(MANDEST)$(PACKAGE).$(MANSECT)
 
+# FIXME: Remove 2009
+# >= 5.10.0-16 has --utf8 option
+POD2MAN		= pod2man
+POD2MAN_FLAGS	= --utf8
+
 makeman: $(MANPAGE)
 
 $(MANPAGE): $(MANPOD)
-	pod2man --center="$(PODCENTER)" \
+	LC_ALL= LANG=C $(POD2MAN) $(POD2MAN_FLAGS) \
+		--center="$(PODCENTER)" \
 		--name="$(PACKAGE)" \
 		--section="$(MANSECT)" \
 		$(MANPOD) \
 	| sed 's,[Pp]erl v[0-9.]\+,$(PACKAGE),' \
-	> $(MANPAGE) && \
+	  > $(MANPAGE) && \
 	rm -f pod*.tmp
 
 # End of of Makefile part
